7f66db93b8b934234aad04a33ced67e5105b3f41
戴志聪 <123313537@qq.com>
2019-05-27 18:47:22 +0800

    edit config

--- admin/config/database.php
+++ admin/config/database.php
@@ -3,13 +3,13 @@
 return [
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'rm-bp1wzzm936044w4gpeo.mysql.rds.aliyuncs.com',
+    'hostname'        => 'rm-bp1wzzm936044w4gpeo42c.mysql.rds.aliyuncs.com',
     // 数据库名
     'database'        => 'webcron',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'DZCqq123313537',
+    'password'        => 'DZC425qq123313537',
     // 端口
     'hostport'        => '3306',
     //调试

719c8f2eee1c49efa815751af795a9e54eb2a216
jonah.fu <ferock@gmail.com>
2019-06-18 18:59:32 +0800

    update code

--- core/process/cronProcess.php
+++ core/process/cronProcess.php
@@ -22,8 +22,8 @@ class cronProcess extends abstractProcess
     public function run($arg)
     {
         //TODO  开启进程读取
-        swoole_timer_after($arg['task_next_exec_time'] - time() , function () use ($arg){
-            echo PHP_EOL."测试成功,调用命令->".$arg['cmd']."执行时间:".date("Y-m-d H:i:s").PHP_EOL;
+        swoole_timer_after($arg['task_next_exec_time'] - time(), function () use ($arg) {
+            echo PHP_EOL . "测试成功,调用命令->" . $arg['cmd'] . "执行时间:" . date("Y-m-d H:i:s") . PHP_EOL;
 //            $this->getProcess()->write("1111");
 //            Di::shareInstance()->get(Di::DI_LOG)->log('1111');
         });
@@ -32,8 +32,8 @@ class cronProcess extends abstractProcess
     public function onReceive(string $str)
     {
         // TODO: Implement onReceive() method.
-        echo "receive:".$str;
-        Di::shareInstance()->get(Di::DI_LOG)->log("接受管道：".$str);
+        echo "receive:" . $str;
+        Di::shareInstance()->get(Di::DI_LOG)->log("接受管道：" . $str);
     }
 
 }
